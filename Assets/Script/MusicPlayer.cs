30febeb8009379f89c6ecd75e1b8e834c68204c4
Apurv singh <apurvsingh9713@gmail.com>
2020-08-16 00:50:01 +0530

    polish terrain splash screen camera scene

---
+++ Assets/Script/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadMainScene",2f);
+    }
+    void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
