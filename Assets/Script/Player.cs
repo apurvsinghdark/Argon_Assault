95d2a6ebed841fe7718f1c750d51a8ee3df4a099
Apurv singh <apurvsingh9713@gmail.com>
2020-08-16 17:53:14 +0530

    movement

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")][SerializeField] float xRange = 5f;
+    void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xNewPos = Mathf.Clamp(transform.localPosition.x + xOffset, -xRange, xRange);
+
+
+        transform.localPosition = new Vector3(xNewPos, transform.localPosition.y,transform.localPosition.z);
+    }
+}
